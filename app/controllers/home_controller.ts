3b513acaa183956895d4750613aa42a7d7a10854
ging1989 <ifreakinn@gmail.com>
2026-04-24 17:17:44 +0700

    add time slot to home

--- app/controllers/home_controller.ts
+++ app/controllers/home_controller.ts
@@ -4,6 +4,25 @@ import Court from '#models/court'
 export default class HomeController {
     async index({view}: HttpContext) {
         const courts = await Court.query().orderBy('id', 'asc')
-        return view.render('pages/home', {courts})
+
+        const hours = [
+            '07:00', '08:00', '09:00', '10:00', '11:00', 
+            '12:00', '13:00', '14:00', '15:00', '16:00', 
+            '17:00', '18:00', '19:00', '20:00', '21:00', '22:00',
+        ]
+        
+        const courtSlots = courts.map((court) => {
+            return {
+                court,
+                slots: hours.map((hour) => {
+                    return {
+                        time: hour,
+                        available: court.status === 'available',
+                    }
+                })
+            }
+        })
+        return view.render('pages/home', {courtSlots})
+        
     }
 }
