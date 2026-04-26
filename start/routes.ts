468df6fbd7a2278c20ec35c78be3c182ffd570a2
garingz <ifreakinn@gmail.com>
2026-04-26 22:04:40 +0700

    add booking page

--- start/routes.ts
+++ start/routes.ts
@@ -10,6 +10,12 @@
 import router from '@adonisjs/core/services/router'
 
 const HomeController = () => import('#controllers/home_controller')
+const CourtsController = () => import('#controllers/courts_controller')
+const BookingsController =() => import('#controllers/bookings_controller')
 
 
 router.get('/', [HomeController, 'index']).as('home')
+
+router.get('/court', [CourtsController, 'index']).as('courts')
+
+router.get('/booking', [BookingsController, 'index']).as('bookings')
